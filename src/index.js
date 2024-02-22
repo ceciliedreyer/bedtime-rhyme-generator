7aed4fa7f24fdde99f9357a5af42c9d66adf1836
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-02-22 14:09:29 +0100

    added the ai api so the rhyme generator now works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-function generateRhyme(event) {
-  event.preventDefault();
+function displayRhyme(response) {
+  console.log("rhyme generated");
 
   new Typewriter("#rhyme", {
-    strings: "hello world",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
   });
+}
+
+function generateRhyme(event) {
+  event.preventDefault();
+
+  let instructionElement = document.querySelector("#instructions");
+
+  let apiKey = "c0f7a728c0575391764t3b111d69od7f";
+  let apiPrompt = `Please create a sleepy bedtime rhyme about a stuffed animal described as: ${instructionElement.value} to help a small child fall asleep`;
+  let apiContext =
+    "You are a an AI assistant who could have worked in a nursery. You love coming up with nursery rhymes in that help little kids calm down and fall asleep. You recieve information about a stuffed animal the child knows and you will then generate a bedtime nursery rhyme with the stuffed animal as the lead character. For example the stuffed animal described is a soft elefant that plays music. Your response could be If you’re silent and don’t speak a word Quiet as a mouse. The elephant is snoring loudly all throughout the house. Like a jet plane in the clouds. Your rhyme is never more than 6 lines long and never shorter than 2. You deliver the rhyme in basic HTML structure and you seperate each line with a <br />. You never add a title to the rhyme you only respons with the lenght of the rhyme.";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRhyme);
+
+  console.log(apiContext);
+  console.log(apiPrompt);
+  console.log("generating rhyme");
 }
 
 let rhymeGeneratorElement = document.querySelector("#rhyme-generator-form");
